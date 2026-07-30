4c560f3ebca20f4ae26db9b3790c14288733a463
arcade_kappa <gkappa@outlands.top>
2026-07-30 20:53:30 +0800

    Remove uesless server thread set from SideTracker

--- src/main/java/li/cil/oc/util/SideTracker.java
+++ src/main/java/li/cil/oc/util/SideTracker.java
@@ -2,21 +2,12 @@ package li.cil.oc.util;
 
 import net.minecraftforge.fml.common.FMLCommonHandler;
 
-import java.util.Collections;
-import java.util.Set;
-
 public final class SideTracker {
-    private static final Set<Thread> serverThreads = Collections.newSetFromMap(new java.util.WeakHashMap<Thread, Boolean>());
-
-    public static void addServerThread() {
-        serverThreads.add(Thread.currentThread());
-    }
-
     public static boolean isServer() {
-        return FMLCommonHandler.instance().getEffectiveSide().isServer() || serverThreads.contains(Thread.currentThread());
+        return FMLCommonHandler.instance().getEffectiveSide().isServer();
     }
 
     public static boolean isClient() {
-        return !isServer();
+        return FMLCommonHandler.instance().getEffectiveSide().isClient();
     }
 }
